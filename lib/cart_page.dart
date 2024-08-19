846094e56736f34565fd64c7ad3e0393ba6764c3
Julian Koripamo <jkoripamo@gmail.com>
2024-08-19 02:37:45 +0100

    calculate total function and clear cart button

--- lib/cart_page.dart
+++ lib/cart_page.dart
@@ -50,6 +50,14 @@ class _CartPageState extends State<CartPage> {
     });
   }
 
+  double calculateTotal() {
+    double total = 0;
+    for (var item in _currentCart) {
+      total += item['price'] * item['unit'];
+    }
+    return total;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -89,88 +97,154 @@ class _CartPageState extends State<CartPage> {
           )
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const SizedBox(height: 25),
-            const Text(
-              'YOUR BAG',
-              style: TextStyle(
-                fontSize: 35,
-                fontWeight: FontWeight.w400,
-                letterSpacing: 1,
-              ),
-            ),
-            const SizedBox(height: 35),
-            _currentCart.isEmpty
-                ? Padding(
-                    padding: const EdgeInsets.all(50),
-                    child: Center(
-                      child: Column(
-                        children: [
-                          const Text(
-                            'Your bag is empty.',
-                            style: TextStyle(fontSize: 15),
-                          ),
-                          const SizedBox(height: 20),
-                          MaterialButton(
-                            color: Theme.of(context).primaryColor,
-                            onPressed: refillCart,
-                            child: const Text(
-                              'Refill',
-                              style: TextStyle(
-                                color: Colors.white,
-                                letterSpacing: 1,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Column(
+              children: [
+                const SizedBox(height: 25),
+                const Text(
+                  'YOUR BAG',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                    letterSpacing: 1,
+                  ),
+                ),
+                const SizedBox(height: 35),
+                _currentCart.isEmpty
+                    ? Padding(
+                        padding: const EdgeInsets.all(50),
+                        child: Center(
+                          child: Column(
+                            children: [
+                              const Text(
+                                'Your bag is empty.',
+                                style: TextStyle(fontSize: 15),
                               ),
-                            ),
-                          )
-                        ],
+                              const SizedBox(height: 20),
+                              MaterialButton(
+                                color: Theme.of(context).primaryColor,
+                                onPressed: refillCart,
+                                child: const Text(
+                                  'Refill',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    letterSpacing: 1,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )
+                    : Column(
+                        children: _currentCart.map((cartItem) {
+                          final String? name = cartItem['name'];
+                          final String? image = cartItem['image'];
+                          final double? price = cartItem['price'];
+                          int unit = cartItem['unit'];
+
+                          // Remove Cart Item
+                          void removeItem() {
+                            setState(() {
+                              _currentCart.remove(cartItem);
+                            });
+                          }
+
+                          // Increase Cart Item
+                          void increment() {
+                            setState(() {
+                              unit++;
+                              cartItem['unit'] = unit;
+                            });
+                          }
+
+                          // Decrease Cart Item
+                          void decrement() {
+                            setState(() {
+                              if (unit > 1) {
+                                unit--;
+                              }
+                              cartItem['unit'] = unit;
+                            });
+                          }
+
+                          return CartCard(
+                            name: '$name',
+                            image: '$image',
+                            price: '$price',
+                            unit: '$unit',
+                            onRemove: removeItem,
+                            onAdd: increment,
+                            onMinus: decrement,
+                          );
+                        }).toList(),
+                      ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                const Divider(),
+                const SizedBox(height: 5),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      'Total',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 1,
                       ),
                     ),
-                  )
-                : Column(
-                    children: _currentCart.map((cartItem) {
-                      final String? name = cartItem['name'];
-                      final String? image = cartItem['image'];
-                      final double? price = cartItem['price'];
-                      int unit = cartItem['unit'];
-
-                      // Remove Cart Item
-                      void removeItem() {
-                        setState(() {
-                          _currentCart.remove(cartItem);
-                        });
-                      }
-
-                      // Increase Cart Item
-                      void increment() {
-                        setState(() {
-                          unit++;
-                          cartItem['unit'] = unit;
-                        });
-                      }
-
-                      // Decrease Cart Item
-                      void decrement() {
-                        setState(() {
-                          unit--;
-                          cartItem['unit'] = unit;
-                        });
-                      }
-
-                      return CartCard(
-                        name: '$name',
-                        image: '$image',
-                        price: '$price',
-                        unit: '$unit',
-                        onRemove: removeItem,
-                        onAdd: increment,
-                        onMinus: decrement,
-                      );
-                    }).toList(),
-                  )
-          ],
-        ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(4)),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 2,
+                          horizontal: 5,
+                        ),
+                        child: Text(
+                          '\$${calculateTotal().toStringAsFixed(2)}',
+                          style: const TextStyle(
+                            color: Colors.white,
+                            letterSpacing: 1,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10, top: 10),
+                  child: MaterialButton(
+                    color: Colors.indigoAccent.shade100,
+                    onPressed: () {
+                      setState(() {
+                        _currentCart.clear();
+                      });
+                    },
+                    child: const Text(
+                      'Clear Cart',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        letterSpacing: 1,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
